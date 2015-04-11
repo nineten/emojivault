836862b203987ac94f20ff5fcefd1e255dfef720
nineten <jayden87@gmail.com>
2015-04-11 16:32:44 +0800

    added annotate

--- app/models/emoji.rb
+++ app/models/emoji.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: emojis
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  img        :binary
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Emoji < ActiveRecord::Base
 end
--- test/models/emoji_test.rb
+++ test/models/emoji_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: emojis
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  img        :binary
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class EmojiTest < ActiveSupport::TestCase
